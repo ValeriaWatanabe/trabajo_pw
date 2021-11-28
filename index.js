cafbac32bb9ea3cb206b2bcb8a0dc7e07f09d439
Rzmn07 <79650211+Rzmn07@users.noreply.github.com>
2021-11-28 14:57:26 -0500

    modified:   dao/models/partida.js 	modified:   index.js 	renamed:    views/partida_admin.ejs -> views/partida-ad-avanzada.ejs 	new file:   views/partida-ad-crear.ejs 	new file:   views/partida-ad-editar.ejs 	new file:   views/partida-ad-simple.ejs 	deleted:    views/partida_crear.ejs 	deleted:    views/partida_editar.ejs 	deleted:    views/partida_eliminar.ejs

--- index.js
+++ index.js
@@ -129,6 +129,178 @@ app.get('/categoria/eliminar/:codigo', async (req, res) => {
     res.redirect('categorias-juego')
 })
 
+//-----------------------------------------------------------
+//---------------------Vista Simple--------------------------
+//-----------------------------------------------------------
+app.get('/partidas/admin', async (req, res) => {
+    const partidas = await db.Partida.findAll({
+        order : [
+            ['fecha','hora_inicio','DESC']
+        ]
+    });
+
+
+    let listaSimplePartida = []
+    for (let partida of partidas) {
+        const juego = await partida.getJuego()
+        listaSimplePartida.push({
+            id : partida.id,
+            juego : juego.nombre,
+            fecha : partida.fecha,
+            hora_inicio : partida.hora_inicio,
+            duracion : partida.duracion,
+            estado : partida.estado
+        })
+    }    
+
+    res.render('partidas-ad-simple', {
+        partidas : listaSimplePartida
+    })
+})
+
+//-----------------------------------------------------------
+//---------------------Vista Avanzada------------------------
+//-----------------------------------------------------------
+app.get('/partidas/admcompleta', async (req, res) => {
+    const partidas = await db.Partida.findAll({
+        order : [
+            ['fecha','hora_inicio','DESC']
+        ]
+    });
+
+
+    let listaAvPartida = []
+    for (let partida of partidas) {
+        const juego = await partida.getJuego()
+        listaAvPartida.push({
+            id : partida.id,
+            juego : juego.nombre,
+            fecha : partida.fecha,
+            hora_inicio : partida.hora_inicio,
+            duracion : partida.duracion,
+            estado : partida.estado,
+            equipo_A : partida.equipo_A,
+            equipo_B : partida.equipo_B,
+            factor_A : partida.factor_A,
+            factor_X : partida.factor_X,
+            factor_B : partida.factor_B,
+            resultado : partida.resultado
+        })
+    }    
+
+    res.render('partidas-ad-avanzada', {
+        partidas : listaAvPartida
+    })
+})
+
+//-----------------------------------------------------------
+//---------------------CREAR PARTIDA-------------------------
+//-----------------------------------------------------------
+app.get('/partidas/admin/crear', async (req, res) => {
+    const partida = await db.Partida.findAll()
+
+    res.render('partida-ad-crear',{
+        partida : partida
+    })
+})
+
+app.post('/partida/admin/crear', async (req, res) => {
+    const partidaJuegoId = req.body.partida_juego_id
+    const partidaFecha = req.body.partida_fecha
+    const partidaHoraInicio = req.body.partida_hora_inicio
+    const partidaDuracion = req.body.partida_duracion
+    const partidaEstado = req.body.partida_estado
+    const partidaEA = req.body.partida_ea
+    const partidaEB = req.body.partida_eb
+    const partidaFA = req.body.partida_fa
+    const partidaFX = req.body.partida_fx
+    const partidaFB = req.body.partida_fb
+    const partidaResultado = req.body.partida_resultado
+
+
+    await db.Partida.create({
+        id_juego : partidaJuegoId,
+        fecha : partidaFecha,
+        hora_inicio : partidaHoraInicio,
+        duracion : partidaDuracion,
+        estado : partidaEstado,
+        equipo_A : partidaEA,
+        equipo_B : partidaEB,
+        factor_A : partidaFA,
+        factor_X : partidaFX,
+        factor_B : partidaFB,
+        resultado : partidaResultado
+    })
+
+    res.redirect('/partidas/admin')
+})
+
+//-----------------------------------------------------------
+//--------------------EDITAR PARTIDA-------------------------
+//-----------------------------------------------------------
+app.get('/partidas/admin/editar/:codigo', async (req, res) => {
+    const id_partida = req.params.codigo
+
+    const partida = await db.Partida.findOne({
+        where : {
+            id : id_partida
+        }
+    })
+    res.render('partida-ad-editar', {
+        partida : partida
+    })
+})
+
+app.post('/partidas/editar', async (req, res) => {
+    const partidaJuegoId = req.body.partida_juego_id
+    const partidaFecha = req.body.partida_fecha
+    const partidaHoraInicio = req.body.partida_hora_inicio
+    const partidaDuracion = req.body.partida_duracion
+    const partidaEstado = req.body.partida_estado
+    const partidaEA = req.body.partida_ea
+    const partidaEB = req.body.partida_eb
+    const partidaFA = req.body.partida_fa
+    const partidaFX = req.body.partida_fx
+    const partidaFB = req.body.partida_fb
+    const partidaResultado = req.body.partida_resultado
+
+    const partida = await db.Partida.findOne({
+        where : {
+            id : id_partida
+        }
+    })
+    partida.id_juego = partidaJuegoId
+    partida.fecha = partidaFecha
+    partida.hora_inicio = partidaHoraInicio
+    partida.duracion = partidaDuracion
+    partida.estado = partidaEstado
+    partida.equipo_A = partidaEA
+    partida.equipo_B = partidaEB
+    partida.factor_A = partidaFA
+    partida.factor_X = partidaFX
+    partida.factor_B = partidaFB
+    partida.resultado = partidaResultado
+
+    await partida.save()
+
+    res.redirect('/partidas/admin')
+
+})
+
+//-----------------------------------------------------------
+//--------------------ELIMINAR PARTIDA-----------------------
+//-----------------------------------------------------------
+app.get('/partidas/admin/eliminar/:codigo', async (req, res) => {
+    const id_partida = req.params.codigo
+    await db.Partida.destroy({
+        where : {
+            id : id_partida
+        }
+    })
+
+    res.redirect('/partidas/admin')
+})
+
 app.listen(PORT, () => {
     console.log('Se ha iniciado el servidor en el puerto ' + PORT)
 })
